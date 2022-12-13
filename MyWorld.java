ae77b1b55bbb6c34f97911b70854a4c7179403ad
haneesh1308 <117936972+haneesh1308@users.noreply.github.com>
2022-12-13 13:19:56 -0500

    Make apple fall from top of the screen

--- MyWorld.java
+++ MyWorld.java
@@ -21,9 +21,17 @@ public class MyWorld extends World
         Elephant elephant = new Elephant();
         addObject(elephant, 300, 200);
         
-        Apple apple = new Apple();
-        addObject(apple, 300, 0);
+        createApple();
     }
     
+    /**
+     * Creats an apple at a random area at the top of the screen
+     */
     public void createApple()
+    {
+       Apple apple = new Apple();
+       int x = Greenfoot.getRandomNumber(600);
+       int y = 0;
+       addObject(apple, 300, 0); 
+    }
 }
