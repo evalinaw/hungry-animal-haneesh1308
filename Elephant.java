044aa2ce923f23e4e213a8e1a98fd8ab7df24334
haneesh1308 <117936972+haneesh1308@users.noreply.github.com>
2022-12-13 14:30:48 -0500

    changed background and elephant texture

--- Elephant.java
+++ Elephant.java
@@ -8,10 +8,29 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Elephant extends Actor
 {
+    GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
+    GreenfootImage idle = new GreenfootImage[8];
+    
     /**
-     * Act - do whatever the Elephant wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
+     * Constructor - A code that runs when an object is created
      */
+    public Elephant()
+    {
+        for(int i = 0; i < idle.length; i++)
+        {
+            idle[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+            idle[i].scale(100, 100);
+        }
+        setImage(idle[0]);
+    }
+    
+    int imageIndex = 0;
+    public void animateElephant()
+    {
+        setImage(idle[imageIndex]);
+        imageIndex = (imageIndex + 1) % idle.length;
+    }
+    
     public void act()
     {
         if(Greenfoot.isKeyDown("d"))
@@ -25,6 +44,8 @@ public class Elephant extends Actor
         }
         
         eat();
+        
+        animateElephant();
     }
     
     /**
@@ -39,6 +60,7 @@ public class Elephant extends Actor
             MyWorld world = (MyWorld) getWorld();
             world.createApple();
             world.increaseScore();
+            elephantSound.play();
         }
     }
 }
