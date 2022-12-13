92cc13a6727b0b6e5ded3cbe82e07ff4b481aa05
haneesh1308 <117936972+haneesh1308@users.noreply.github.com>
2022-12-13 13:39:00 -0500

    Added game over screen

--- Apple.java
+++ Apple.java
@@ -11,6 +11,15 @@ public class Apple extends Actor
     
     public void act()
     {
+        int x = getX();
+        int y = getY() + 2;
         setLocation(getX(), getY() + 1);
+        
+        MyWorld world = (MyWorld) getWorld();
+        if(getY() >= world.getHeight())
+        {
+            world.gameOver();
+            world.removeObject(this);
+        }
     }
 }
