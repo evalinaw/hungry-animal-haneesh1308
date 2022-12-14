59c589910e86a7f98d766859709f38d5d9802ab1
haneesh1308 <117936972+haneesh1308@users.noreply.github.com>
2022-12-13 19:13:30 -0500

    Created new title screen and instructions to game

--- TItleScreen.java
+++ TItleScreen.java
@@ -33,6 +33,7 @@ public class TitleScreen extends World
             Greenfoot.setWorld(gameWorld);
         }
     }
+    
     /**
      * Prepare the world for the start of the program.
      * That is: create the initial objects and add them to the world.
@@ -41,5 +42,20 @@ public class TitleScreen extends World
     {
         Elephant elephant = new Elephant();
         addObject(elephant,490,117);
+        Label label = new Label("Press <space> to Start", 40);
+        addObject(label,303,295);
+        elephant.setLocation(515,122);
+        label.setLocation(345,341);
+        label.setLocation(330,338);
+        Label label2 = new Label("Press \u27f5 to move left and \u27f6 to move right", 30);
+        addObject(label2,280,228);
+        label2.setLocation(319,239);
+        label.setLocation(316,315);
+        label2.setLocation(326,219);
+        label.setLocation(305,297);
+        label.setLocation(246,339);
+        elephant.setLocation(302,100);
+        label2.setLocation(305,288);
+        label.setLocation(306,358);
     }
 }
